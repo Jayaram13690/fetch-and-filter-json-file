67905c5367a21e5512d1d7bddf9c2381e0e69065
Jayaram13690 <jayaram13690@gmail.com>
2026-05-17 17:47:09 +0530

    Updated load_data() to display default data in json.

--- json_load_app.py
+++ json_load_app.py
@@ -1,7 +1,19 @@
 import json
 
+
+def load_data(data):
+    print("Default Data:")
+    for person in data:
+        print(f"ID: {person['id']}")
+        print(f"Name: {person['name']}")
+        print(f"Age: {person['age']}")
+        print(f"City: {person['city']}")
+        print("------------")
+
 with open('data.json', 'r') as file:
     data = json.load(file) #loads the json file data to python list.
+    
+    load_data(data)
 
 if isinstance(data, list):
     filtered_data = []
